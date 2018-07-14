0b2435bd92d26807394ba870992c093b35ec9633
Manish Munikar <munikarmanish@gmail.com>
2018-07-14 21:45:42 +0545

    exercise: basic auth

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/users');
@@ -10,14 +11,10 @@ const dishRouter = require('./routes/dishes');
 const promotionRouter = require('./routes/promotions');
 const leaderRouter = require('./routes/leaders');
 
-const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost:54321/conFusion', { useNewUrlParser: true })
     .then(() => {
         console.log('Successfully connected to MongoDB server');
-    }).catch(err => {
-        console.log(err);
-    });
+    }).catch(err => console.log(err));
 
 const app = express();
 
@@ -29,6 +26,30 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Authorization middleware
+function auth(req, res, next) {
+    console.log(req.headers);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        const err = new Error('You are not authenticated');
+        err.status = 401;
+        res.setHeader('WWW-Authenticate', 'Basic');
+        next(err);
+    } else {
+        const [username, password] = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+        if (username === 'admin' && password === 'password') {
+            next();
+        } else {
+            const err = new Error('You are not authenticated');
+            err.status = 401;
+            res.setHeader('WWW-Authenticate', 'Basic');
+            next(err);
+        }
+    }
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -38,12 +59,12 @@ app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res) {
+app.use(function(err, req, res) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
