fffefb409eda521b4460f057c2cb7ddb91cb3d23
Manish Munikar <munikarmanish@gmail.com>
2018-07-14 22:58:14 +0545

    exercise: cookies

--- app.js
+++ app.js
@@ -25,20 +25,33 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 // Authorization middleware
 function auth(req, res, next) {
-    console.log(req.headers);
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        const err = new Error('You are not authenticated');
-        err.status = 401;
-        res.setHeader('WWW-Authenticate', 'Basic');
-        next(err);
+    console.log(req.signedCookies);
+
+    if (!req.signedCookies.user) {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            const err = new Error('You are not authenticated');
+            err.status = 401;
+            res.setHeader('WWW-Authenticate', 'Basic');
+            next(err);
+        } else {
+            const [username, password] = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+            if (username === 'admin' && password === 'password') {
+                res.cookie('user', 'admin', { signed: true });
+                next();
+            } else {
+                const err = new Error('You are not authenticated');
+                err.status = 401;
+                res.setHeader('WWW-Authenticate', 'Basic');
+                next(err);
+            }
+        }
     } else {
-        const [username, password] = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-        if (username === 'admin' && password === 'password') {
+        if (req.signedCookies.user === 'admin') {
             next();
         } else {
             const err = new Error('You are not authenticated');
@@ -48,6 +61,7 @@ function auth(req, res, next) {
         }
     }
 }
+
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
